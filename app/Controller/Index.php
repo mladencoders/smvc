cd8c5a17f98d5d2217c5ab1d89b7cf705e80041a
Mladen Ilic <mladens.world@gmai.com>
2014-07-24 01:04:41 +0200

    example views updated

--- app/Controller/Index.php
+++ app/Controller/Index.php
@@ -1,12 +1,7 @@
 <?php
 
 class Controller_Index extends Smvc_Controller
-{   
-    function __construct() 
-    {
-
-    }
-    
+{      
     public function index()
     {
         $this->getView()->setData("message", "message");
