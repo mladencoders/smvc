3afde645538480db080bd566c2121811f9dfbae7
Mladen Ilic <mladens.world@gmai.com>
2014-07-24 01:04:18 +0200

    Set project root

--- lib/Smvc/Smvc.php
+++ lib/Smvc/Smvc.php
@@ -3,14 +3,34 @@ require_once "Smvc/Autoloader.php";
 
 class Smvc
 {   
+    private static $_root;
+    private static $_paths = array();
+    
     public function bootstrap()
     {   
-        
         $autoloader = new Autoloader();
         $autoloader->init();
         
         $dispatcher = new Smvc_Dispatcher();        
-        $dispatcher->run();
-        
-    }   
+        $dispatcher->run();  
+    }
+
+    public static function setRoot($root)
+    {
+        self::$_root = $root;
+        self::$_paths['app'] = self::getRootDirPath() . DS . "app";
+        self::$_paths['config'] = self::getRootDirPath() . DS . "config";
+        self::$_paths['skin'] = self::getRootDirPath() . DS . "skin";
+        self::$_paths['lib'] = self::getRootDirPath() . DS . "lib";
+    }
+    
+    public static function getRootDirPath()
+    {
+        return self::$_root;
+    }
+    
+    public static function getDirPath($dir)
+    {
+        return self::$_paths[$dir];
+    }
 }
