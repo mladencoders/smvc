cab56b67a71cfc20499f8aeec548e112a0dc747a
Mladen Ilic <mladens.world@gmai.com>
2014-11-08 08:42:57 +0100

    Adjust testing example for new request object

--- app/Test/Controller/Index.php
+++ app/Test/Controller/Index.php
@@ -4,11 +4,7 @@ class Test_Controller_Index extends Smvc_Controller
 {      
     public function index()
     {
-        $session = new Smvc_Session();
-        $session->start();
-        $name = $session->getData("auth");
-        Smvc_App::getConfig("database", "host");
-        
+        $this->getView()->setData("title", $this->getRequest()->getParam("test"));
         $this->getView()->render();
     }
     
