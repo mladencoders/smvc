ecdfd300ba487efdfe428d67ecf7f29a730d45a3
Mladen Ilic <mladens.world@gmai.com>
2014-11-09 17:40:33 +0100

    Refactor Smvc_Log

--- lib/Smvc/Log.php
+++ lib/Smvc/Log.php
@@ -2,56 +2,22 @@
 
 class Smvc_Log
 {
-    protected $_path = "";
-    protected $_enabled = 0;
-    
-    public function __construct($path)
-    {      
-        $this->_path = Smvc_App::getDirPath() . DS . ltrim("\\/", $path);
-        if (! file_exists(dirname($logPath))) {
-            mkdir(dirname($logPath), 0777, true);
-        }
-        
-        file_put_contents($this->_path, "", FILE_APPEND);
+    public static function log($message, $logType = 'system-log')
+    {
+        self::logRaw(date(Smvc_App::getConfig($logType, "time_format")) . " - " . $message, $logType);
     }
     
-    public function userLog($message) 
+    public static function logRaw($message, $logType = 'system-log')
     {
-        file_put_contents(
-            $this->_path,
-            date(Smvc_App::getConfig("log", "time_format")) . 
-            " - " . 
-            $message . 
-            PHP_EOL, 
-            FILE_APPEND
-        );
-    }
-    
-    public static function log($message)
-    {
-        self::logRaw(date(Smvc_App::getConfig("log", "time_format")) . " - " . $message);
-    }
-    
-    public static function logRaw($message)
-    {
-        if ((int)Smvc_App::getConfig("log", "enabled") !== 1) {
+        if ((int)Smvc_App::getConfig($logType, "enabled") !== 1) {
             return;
         }
         
-        $logPath = Smvc_App::getDirPath() . Smvc_App::getConfig("log", "path");
+        $logPath = Smvc_App::getDirPath() . Smvc_App::getConfig($logType, "path");
         if (! file_exists(dirname($logPath))) {
             mkdir(dirname($logPath), 0777, true);
         }
         
         file_put_contents($logPath, $message.PHP_EOL, FILE_APPEND);
-    }
-    
-    protected function _isEnabled()
-    {
-        if (!isset($this->_enabled)) {
-            $this->_enabled = (int)Smvc_App::getConfig("log", "enabled") === 1;
-        }
-        
-        return $this->_enabled;
     }
 }
